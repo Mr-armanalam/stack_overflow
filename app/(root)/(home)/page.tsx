eeb26eecd8fc3317dce32c02391529d43f83addc
Arman Alam <armanalam78578@gmail.com>
2024-11-02 08:30:55 +0530

    implement theme provider

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,17 +1,17 @@
-import { SignedIn, SignedOut, SignInButton, UserButton } from '@clerk/nextjs'
-import React from 'react'
+import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
+import React from "react";
 
 const Home = () => {
   return (
-    <div>Home
-        <SignedOut> 
-            <SignInButton />
-          </SignedOut>
-           <SignedIn>
-            <UserButton />
-          </SignedIn>
+    <div>
+      <SignedOut>
+        <SignInButton fallbackRedirectUrl={"/"} />
+      </SignedOut>
+      <SignedIn>
+        <UserButton />
+      </SignedIn>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
