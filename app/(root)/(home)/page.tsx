dab86419590aff13252998ed3a5e2082c744e02b
Arman Alam <armanalam78578@gmail.com>
2024-11-02 14:21:09 +0530

    navbar and mobilenav now completed

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,15 +1,10 @@
-import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
+// import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
 import React from "react";
 
 const Home = () => {
   return (
-    <div>
-      <SignedOut>
-        <SignInButton fallbackRedirectUrl={"/"} />
-      </SignedOut>
-      <SignedIn>
-        <UserButton />
-      </SignedIn>
+    <div className="text-white">
+      home
     </div>
   );
 };
