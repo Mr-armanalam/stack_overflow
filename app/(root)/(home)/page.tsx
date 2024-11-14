af95d682ecce278fe078d54f36b215f22914488e
Arman Alam <armanalam78578@gmail.com>
2024-11-14 06:03:30 +0530

    metadata_completed

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -11,6 +11,11 @@ import { SearchParamsProps } from "@/types";
 import Link from "next/link";
 import React from "react";
 
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Home | Dev Overflow",
+}
 
 const Home = async ({ searchParams }: SearchParamsProps) => {
   const searchparams = await searchParams;
